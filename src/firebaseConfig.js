f44b9062551dac11b81141b8822735c83f315994
tranduy <tranbaoduy4@gmail.com>
2022-06-15 00:54:17 +0700

    fix .env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,22 @@
 import {initializeApp} from "firebase/app";
 import {getStorage} from 'firebase/storage';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyA0Psy4GpZ_ls5yzpspQg5q5BMdSLbVyuQ",
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
+    /*
     authDomain: "story-e3425.firebaseapp.com",
     projectId: "story-e3425",
     storageBucket: "story-e3425.appspot.com",
     messagingSenderId: "209959015986",
     appId: "1:209959015986:web:7960159edfaf0a8ab70e2b",
     measurementId: "G-FSH6HQ253X"
+    */
 };
 const app = initializeApp(firebaseConfig);
 
