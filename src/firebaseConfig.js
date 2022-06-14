509f4adfdcd7bee0a80ea4c611d3f2d6cb1b98e1
tranduy <tranbaoduy4@gmail.com>
2022-06-15 01:11:56 +0700

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,14 +9,7 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
     appId: process.env.REACT_APP_appId,
     measurementId: process.env.REACT_APP_measurementId,
-    /*
-    authDomain: "story-e3425.firebaseapp.com",
-    projectId: "story-e3425",
-    storageBucket: "story-e3425.appspot.com",
-    messagingSenderId: "209959015986",
-    appId: "1:209959015986:web:7960159edfaf0a8ab70e2b",
-    measurementId: "G-FSH6HQ253X"
-    */
+    
 };
 const app = initializeApp(firebaseConfig);
 
